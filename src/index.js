80970872237076f2587be4a7d27ad6eb7174af22
Ville Ristimäki <villeristimaki@gmail.com>
2017-08-23 23:46:17 +0300

    Add simple route

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import 'babel-polyfill';
 import Koa from 'koa';
 import dotenv from 'dotenv';
+import Router from 'koa-better-router';
 
 dotenv.config();
 
 import {getEnv, debug} from './util';
 
-const app = new Koa();
-const PORT = getEnv('PORT') || 3000;
+const routerOpts = {
+  notFound: (ctx, next) => {
+    ctx.status = 404;
+    ctx.body = {msg: `Endpoint: ${ctx.originalUrl} not found!`};
+    return next();
+  },
+};
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello World';
+const PORT = getEnv('PORT') || 3000;
+const router = new Router(routerOpts).loadMethods();
+const app = new Koa();
+
+router.get('/', (ctx, next) => {
+  ctx.body = `Wadaap from koa-api-boilerplate! ${ctx.route.prefix}`;
 });
 
+app.use(router.middleware());
 app.listen(PORT, () => debug(`Your awesome Koa API is running @ http://127.0.0.1:${PORT}`));
