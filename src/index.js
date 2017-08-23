d5bc7eb6ecdd54d5c7a6a6831c5fcc59feea070f
Ville Ristimäki <villeristimaki@gmail.com>
2017-08-23 19:56:26 +0300

    Use callback

--- src/index.js
+++ src/index.js
@@ -11,6 +11,4 @@ app.use(async (ctx) => {
   ctx.body = 'Hello World';
 });
 
-app.listen(PORT);
-
-console.log(`Your awesome Koa API is running @ http://127.0.0.1:${PORT}`);
+app.listen(PORT, () => console.log(`Your awesome Koa API is running @ http://127.0.0.1:${PORT}`));
